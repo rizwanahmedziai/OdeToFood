9e5b39e839fa9ddaf99e3e280e96a6bdb12cc2d4
Rizwan Ahmed <rizwan@zekiexperts.com>
2020-04-11 21:55:09 +0500

    Added In memory Data

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,26 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Pizza Hutt", Cuisine = CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Lasania", Cuisine = CuisineType.Indian},
+                new Restaurant { Id = 3, Name = "Pindi Khokha", Cuisine = CuisineType.French},
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
