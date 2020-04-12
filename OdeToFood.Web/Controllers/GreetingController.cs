7134bdbe972124cfb6d3f98bda8af054f1f011dc
Rizwan Ahmed <rizwan@zekiexperts.com>
2020-04-12 15:23:35 +0500

    Added greetings controller

---
+++ OdeToFood.Web/Controllers/GreetingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using OdeToFood.Web.Models;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class GreetingController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new GreetingViewModel();
+            model.Message = ConfigurationManager.AppSettings["message"];
+
+            
+            return View(model);
+        }
+    }
+}
